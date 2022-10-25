34bebcd5cace27d52a4d64cb68872b7a781b77fc
Perry <petreli1@ymail.com>
2022-10-25 16:28:56 +0100

    trying to get github workflows to work

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,7 @@ const setupDatabase = () => {
   const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     port: DB_PORT,
-    dialect: NODE_ENV === 'test' ?  'mysql' : 'postgres',
+    dialect: 'postgres',
     logging: false,
   });
 
